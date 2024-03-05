cc11cb8f5f2c9f66f9025e13ab3fc3e7e810f701
Slava Anishchuk <appsbaker@mail.ru>
2024-03-05 14:28:45 +0200

    Initial Commit

---
+++ EmptyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmptyApp
+//
+//  Created by Slava Anishchuk on 05.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
